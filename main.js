f9e566e37ff48ed20bdb81fb4e68ef2901ec6130
omarbara <omarbara@ymail.com>
2019-10-28 16:40:16 +0100

    just git with it

---
+++ main.js
@@ -0,0 +1,48 @@
+$('img').attr('src', 'http://t0.gstatic.com/images?q=tbn:ANd9GcSJKLiEyyz1Q9RC8EBYl3ijr3nuGeyO2ETmwy6Kdq0AQtD0elWD')
+
+
+// 4- A. Insert content to the end of an element.
+
+// 5-
+$('#secret').hide()
+
+//6- C. the background-color is not in a string.
+
+// $('body').css('background-color', 'red')
+
+// 8-
+$("ol").append("<li>four thing </li>")
+
+// web dev 2
+// 1- F. Bob says hi, immediately
+// 2-
+function sayHello () {
+	alert(this.name + " says hello!");
+}
+var alice = { name: "Alice" };
+
+sayHello.call(alice);
+
+// 3-
+// result = 20
+
+// 4- A-"Hi I am Alice"
+
+//5 - B.20
+
+//6 - C. Alice says hi, after 1 second
+
+//7 - A: modularize our code for reuse and composition
+
+
+
+////////////// GIT Q
+// 1- C.git clone
+// 2- B.git status
+// 3- C. rm *.json
+// 4- C. rm -rf config
+// 5-
+// 6- B. git remote
+// 7- A. I create the new setup directory, copy all the files from config into it.
+// 8- 1. cd ..
+// 8- 2. cd ../../public
